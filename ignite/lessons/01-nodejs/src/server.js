8949f4af19929520fe74d9c0eb42a6228e6e97ed
Ariane <ariianeboorges@gmail.com>
2023-09-26 17:52:58 +0100

    feat: http methods

--- ignite/lessons/01-nodejs/src/server.js
+++ ignite/lessons/01-nodejs/src/server.js
@@ -1,6 +1,16 @@
 import http from "node:http";
 
 const server = http.createServer((request, response) => {
+  const {method, url} = request;
+
+  if (method === "GET" && url === "/users") {
+    return response.end("List users");
+  }
+
+  if (method === "POST" && url === "/users") {
+    return response.end("Create users");
+  }
+
   return response.end("Hello ignite");
 });
 
